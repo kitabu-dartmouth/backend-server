7db677155c0f6707bdd3f156c3fdc71f137f0523
Prashant <prashant.barca@gmail.com>
2017-02-24 22:10:57 -0500

    Add serializer for user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,58 +1,58 @@
 class ApiController < ApplicationController
-    def sign_in
-        phoneno = params[:phoneno]
-        password = params[:password]
-        user = User.find_by_phoneno(phoneno)
-        if user == nil
-            render json: false
+  def sign_in
+    phoneno = params[:phoneno]
+    password = params[:password]
+    user = User.find_by_phoneno(phoneno)
+    if user == nil
+      render json: false
+    else
+      if user.valid_password? password
+        render json: user
+      else
+        render json: false
+      end
+    end
+
+  end
+
+  def validate_each(value)
+    if value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      true
+    else
+      false
+    end
+  end
+
+  def register
+    phoneno = params[:phoneno]
+    password = params[:password]
+    password_confirmation = params[:password_confirmation]
+    email = params[:email]
+    name = params[:name]
+    if !validate_each(email) or User.find_by_phoneno(phoneno)!=nil
+      render json: false
+    else
+      user = User.new({:phoneno => phoneno, :password => password, :password_confirmation => password_confirmation, :email => email, :name => name})
+      user.save
+      user = User.find_by_phoneno(phoneno)
+      if user == nil
+        render json: false
+      else
+        if user.valid_password? password
+          render json: true
         else
-            if user.valid_password? password
-              render json: true
-            else
-              render json: false
-            end
+          render json: false
         end
-
+      end
     end
+  end
 
-    def validate_each(value)
-        if value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-            true
-        else
-            false
-        end
-    end
+  def add_link
+  end
 
-    def register
-        phoneno = params[:phoneno]
-        password = params[:password]
-        password_confirmation = params[:password_confirmation]
-        email = params[:email]
-        name = params[:name]
-        if !validate_each(email) or User.find_by_phoneno(phoneno)!=nil
-            render json: false
-        else
-            user = User.new({:phoneno => phoneno, :password => password, :password_confirmation => password_confirmation, :email => email, :name => name})
-            user.save
-            user = User.find_by_phoneno(phoneno)
-            if user == nil
-                render json: false
-            else
-                if user.valid_password? password
-                    render json: true
-                else
-                    render json: false
-                end
-            end
-        end
-    end
+  def update_link
+  end
 
-    def add_link
-    end
-
-    def update_link
-    end
-
-    def delete_link
-    end
+  def delete_link
+  end
 end
--- app/serializers/link_serializer.rb
+++ app/serializers/link_serializer.rb
@@ -1,3 +1,3 @@
 class LinkSerializer < ActiveModel::Serializer
-  attributes :id, :url, :type
+  attributes :id, :url, :type, :user_id
 end
